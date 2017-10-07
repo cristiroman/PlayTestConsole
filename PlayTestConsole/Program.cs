409fdd5396fec94bbd9d2b67482e32291e81fe72
Cristi Roman <cristiroman@live.com>
2017-10-07 22:15:35 +0300

    test

---
+++ PlayTestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = 10;
+            var g = 15;
+        }
+    }
+}
